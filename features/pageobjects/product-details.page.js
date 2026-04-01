5bab09384ebf5954e3fc61066d56493534edea75
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-04-01 12:58:11 +0300

    Update product-details.page.js

--- features/pageobjects/product-details.page.js
+++ features/pageobjects/product-details.page.js
@@ -1,28 +1,61 @@
-const { expect } = require("@wdio/globals");
+const { expect, browser } = require("@wdio/globals");
 const Page = require("./page");
+const loginPage = require("./login.page");
 
 class ProductDetails extends Page {
-    get addToCart() { return $('[data-test="add-to-cart"]'); }
-    get successToast() { return $('#toast-container'); }
-    get basketCounter() { return $('[data-test="cart-quantity"]'); }
+    //get successToast() { return $('#toast-container'); }
 
-    async open() {
-        return super.open('product/01KB2FH1GCJ9KQM30564ZVSM97');
+    async goToHomePage() {
+        await $('[data-test="nav-home"]').click();
+        await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/');
+    }
+
+    async clickProduct() {
+        const product = await $('[data-test^="product-"]');
+        await product.waitForClickable({ timeout: 10000 });
+        await product.click();
     }
 
     async clickCartButton() {
-        await this.addToCart.click();
+        const addToCartButton = await $('[data-test="add-to-cart"]');
+        await addToCartButton.click();
     }
 
     async successMessage() {
-        await expect(this.successToast).toBeDisplayed();
-        await expect(this.successToast).toHaveText('Product added to shopping cart.');
+        const successToast = $('#toast-container');
+        await expect(successToast).toBeDisplayed();
+        await expect(successToast).toHaveText('Product added to shopping cart.');
+    }
+
+    async getBasketCount() {
+        const elements = await $$('[data-test="cart-quantity"]');
+        if (elements.length === 0) {
+            return 0;
+        } else {
+            const basketCounter = elements[0];
+            await basketCounter.waitForDisplayed({ timeout: 5000 });
+            const count = await basketCounter.getText();
+            return parseInt(count);
+        }
     }
 
     async counter() {
-        const oldValue = parseInt(await this.basketCounter.getText());
-        const newValue = parseInt(await this.basketCounter.getText());
+        const oldValue = await this.getBasketCount();
+        await this.clickCartButton();
+        await this.successMessage();
 
+        await browser.waitUntil(
+            async () => {
+                const newValue = await this.getBasketCount();
+                return newValue === oldValue + 1;
+            },
+            {
+                timeout: 10000,
+                timeoutMsg: 'Cart counter did not updated!'
+            }
+        );
+
+        const newValue = await this.getBasketCount();
         await expect(newValue).toEqual(oldValue + 1);
     }
 }
