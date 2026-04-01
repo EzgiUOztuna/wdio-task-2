e82d3a5292cd3659bab8ed55613f7c8c3a2132d4
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-04-01 13:51:39 +0300

    Update favorite-products.page.js

--- features/pageobjects/favorite-products.page.js
+++ features/pageobjects/favorite-products.page.js
@@ -1,22 +1,35 @@
+const { expect } = require("@wdio/globals");
 const Page = require("./page");
 
 class FavoriteProducts extends Page {
     get addFavorites() { return $('[data-test="add-to-favorites"]'); }
-    get errorToast() { return $('div[role="alert"][class*="toast-message"]'); }
-
-    async open() {
-        return super.open('product/01KB2FH1GEK977ZNZMDS0WXR9Z');
-    }
+    get successToast() { return $('#toast-container'); }
 
     async clickFavoriteButton() {
         await this.addFavorites.click();
     }
 
-    async errorMessage(expectedMessage) {
-        await expect(this.errorToast).toBeDisplayed();
-        await expect(this.errorToast).toHaveText(expectedMessage);
+    async successMessage(expectedMessage) {
+        await expect(this.successToast).toBeDisplayed();
+        await expect(this.successToast).toHaveText(expectedMessage);
+    }
+
+    async removeFavorites() {
+        const fullnameButton = await $('[data-test="nav-menu"]');
+        await fullnameButton.click();
+
+        const dropdownMenu = await $('.dropdown-menu.show');
+        await expect(dropdownMenu).toBeDisplayed();
+
+        const favoriteButton = await $('[data-test="nav-my-favorites"]');
+        await favoriteButton.click();
+
+        await browser.url('https://practicesoftwaretesting.com/account/favorites');
+
+        const deleteButton = await $('[data-test="delete"]');
+        await deleteButton.click();
+        await expect(deleteButton).not.toExist();
     }
 }
 
 module.exports = new FavoriteProducts();
-
