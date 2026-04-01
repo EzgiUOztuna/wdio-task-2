7d6797686b3396fa15980b216ff5d68b574de887
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-04-01 17:21:09 +0300

    Update product-details.steps.js

--- features/step-definitions/product-details.steps.js
+++ features/step-definitions/product-details.steps.js
@@ -1,30 +1,26 @@
 const { Given, When, Then } = require("@wdio/cucumber-framework");
+const { expect, browser } = require("@wdio/globals");
 const Login = require("../pageobjects/login.page");
 const ProductDetails = require("../pageobjects/product-details.page");
-console.log("STEP FILE LOADED");
 
-Given("the user is on the product details page", async () => {
-    console.log("STEP WORKED");
-    await Login.login("customer3@practicesoftwaretesting.com", "pass123");
+Given("user is on product details page", async () => {
+    await Login.login("customer2@practicesoftwaretesting.com", "welcome01");
     await ProductDetails.goToHomePage();
+    await ProductDetails.clickProduct();
 });
 
-/*
-When("the user clicks on the \'Add to cart\' button", async () => {
+When("user clicks add to cart button", async () => {
     await ProductDetails.clickCartButton();
 });
 
-Then('the product should be added to the basket', async () => {
+Then("product is added to basket", async () => {
     await ProductDetails.successMessage();
 });
 
-Then('the basket counter should increase by one', async () => {
-    await ProductDetails.verifyBasketIncrement();
+Then("basket counter increases", async () => {
+    await ProductDetails.counter();
 });
 
-Then("confirmation message should be displayed saying \'Product added to shopping cart\'", async () => {
+Then("success message is displayed", async () => {
     await ProductDetails.successMessage();
 });
-*/
-
-module.exports = {};
