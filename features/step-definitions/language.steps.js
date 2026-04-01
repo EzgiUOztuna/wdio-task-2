7257289ccd81d4a3617f6f6ff4ec909527bc92a7
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-04-01 13:51:50 +0300

    Update language.steps.js

--- features/step-definitions/language.steps.js
+++ features/step-definitions/language.steps.js
@@ -1,8 +1,11 @@
 const { Given, When, Then } = require("@wdio/cucumber-framework");
 const Language = require("../pageobjects/language.page");
+const Login = require("../pageobjects/login.page");
+const productDetailsPage = require("../pageobjects/product-details.page");
 
 Given("the user is on the home page", async () => {
-    await Language.open();
+    await Login.login("customer2@practicesoftwaretesting.com", "welcome01");
+    await productDetailsPage.goToHomePage();
 });
 
 When("the user clicks on the world icon or 'EN' word", async () => {
