2dd0292a256280aae00f48eba556872c40e6dc64
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-04-01 13:51:47 +0300

    Update favorite-products.steps.js

--- features/step-definitions/favorite-products.steps.js
+++ features/step-definitions/favorite-products.steps.js
@@ -1,19 +1,21 @@
 const { Given, When, Then } = require("@wdio/cucumber-framework");
 const FavoriteProducts = require("../pageobjects/favorite-products.page");
-const { expect, browser } = require("@wdio/globals");
+const Login = require("../pageobjects/login.page");
+const productDetailsPage = require("../pageobjects/product-details.page");
+//console.log("STEP FILE LOADED");
 
 Given("the user is on the product details page", async () => {
-    await FavoriteProducts.open();
+    await Login.login("customer2@practicesoftwaretesting.com", "welcome01");
+    //console.log("STEP WORKED_2");
+    await productDetailsPage.goToHomePage();
+    await productDetailsPage.clickProduct();
 });
-
+/*
 When("the user clicks on the 'Add to favorites' button", async () => {
     await FavoriteProducts.clickFavoriteButton();
 });
 
-Then("an error message should be displayed saying {string}", async (message) => {
-    await FavoriteProducts.errorMessage(message);
-});
-
-Then("no changes should occur on the page", async () => {
-    await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/product/01KB2FH1GEK977ZNZMDS0WXR9Z');
-});
+Then("a success message should be displayed", async () => {
+    await FavoriteProducts.successMessage('Product added to your favorites list.');
+    await FavoriteProducts.removeFavorites();
+});*/
